e7dd30d3cf755746be1ee7387be8e95b98269699
ericfloriano <ericfloriano@gmail.com>
2025-04-08 08:56:35 -0300

    Adicionar exemplo simple3s de dicionario

---
+++ exemplo_dict.py
@@ -0,0 +1,20 @@
+produto_01 = {
+    "nome":"Sapato",
+    "quantidade":25,
+    "preco":10.38,
+    "disponibiidade":True
+}
+
+produto_02 = {
+    "nome":"Camiseta",
+    "quantidade":58,
+    "preco":5.38,
+    "disponibiidade":False
+}
+
+carrinho: list = []
+
+carrinho.append(produto_01)
+carrinho.append(produto_02)
+
+print(carrinho)
