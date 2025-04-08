a3d5790c6070435fd2be6a23e3e83bf17aac19b4
ericfloriano <ericfloriano@gmail.com>
2025-04-08 09:03:22 -0300

    Adicionar mais produtos na lista

--- exemplo_list.py
+++ exemplo_list.py
@@ -1,11 +1,13 @@
 produto: str = "sapato"
 produto_2: str = "camiseta"
 produto_3: str = "videogame"
+produto_4: str = "calca"
 
 produtos: list = []
 
 produtos.append(produto)
 produtos.append(produto_2)
 produtos.append(produto_3)
+produto.append(produto_4)
 
 print(produtos)
