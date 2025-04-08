0254fe3a169d96e476c7ed5a2dbe0cdd853dc967
ericfloriano <ericfloriano@gmail.com>
2025-04-08 08:55:55 -0300

    Adicionar exemplo simples de lista

---
+++ exemplo_list.py
@@ -0,0 +1,11 @@
+produto: str = "sapato"
+produto_2: str = "camiseta"
+produto_3: str = "videogame"
+
+produtos: list = []
+
+produtos.append(produto)
+produtos.append(produto_2)
+produtos.append(produto_3)
+
+print(produtos)
